b0a1834da02123c8eaf2fb4b08c5f718655a0016
Hieu Nguyen <hieundgc01031@fpt.edu.vn>
2018-05-28 23:12:38 +0700

    login

--- js/basejs.js
+++ js/basejs.js
@@ -1,14 +1,45 @@
 $(document).ready(function () {
+    var widwin = $(window).width();
+    widmodal = $('.modal-connect--signup').width();
+    var cent = (widwin - widmodal)/2;
+    $('.modal-connect--signup').css('left', cent);
+    $('.modal-connect--signin').css('left', cent);
 
+    $(window).resize(function () {
+        var widwin = $(window).width();
+        widmodal = $('.modal-connect--signup').width();
+        var cent = (widwin - widmodal)/2;
+        $('.modal-connect--signup').css('left', cent);
+        $('.modal-connect--signin').css('left', cent);
+
+    });
 
     $('.usercon--signup').click(function () {
-       $('.modal-connect--signup').show();
-       $('.modal-connect--signin').hide();
+        $('#modal-signin').modal('hide');
+        $('#modal-signup').modal('show');
+        event.stopPropagation();
+
+
+
     });
 
     $('.usercon--signin').click(function () {
-        $('.modal-connect--signin').show();
-        $('.modal-connect--signup').hide();
+        $('#modal-signin').modal('show');
+        $('#modal-signup').modal('hide');
+        event.stopPropagation();
     });
+
+    $('html').click(function() {
+        $('#modal-signin').modal('hide');
+        $('#modal-signup').modal('hide');
+    });
+
+    $('#modal-signin').click(function(event){
+        event.stopPropagation();
+    });
+    $('#modal-signup').click(function(event){
+        event.stopPropagation();
+    });
+
 
 });
